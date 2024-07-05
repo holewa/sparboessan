fb57965b5487cd846a2990dc8623f456c45e648a
joel holewa <holewa@gmail.com>
2024-07-05 12:04:07 +0200

    datumlogik

--- lib/providers/date_provider.dart
+++ lib/providers/date_provider.dart
@@ -4,14 +4,18 @@ import 'package:flutter/material.dart';
 
 class DateProvider extends ChangeNotifier {
   DateTime _currentTime = DateTime.now();
+  int _currentDay = DateTime.now().day;
+
   Timer? _timer;
 
   DateProvider() {
     _startTimer();
   }
 
-  DateTime get currentTime => _currentTime;
   String get currentDay => getDayOfTheWeek(_currentTime);
+
+  String get daysUntilSaturdayText =>
+      _daysUntilSaturdayText(_daysUntilSaturday());
 
   void _startTimer() {
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
@@ -38,4 +42,25 @@ String getDayOfTheWeek(DateTime dateTime) {
 
 String dayToUpperString(String day) {
   return day.substring(0, 1).toUpperCase() + day.substring(1);
+}
+
+int _daysUntilSaturday() {
+  int saturday = DateTime.saturday;
+  int daysUntilSaturday = saturday - DateTime.now().day;
+  return daysUntilSaturday;
+}
+
+String _daysUntilSaturdayText(daysUntilSaturday) {
+  if (daysUntilSaturday == 1) {
+    return '$daysUntilSaturday dag kvar till veckopeng!';
+  }
+  if (daysUntilSaturday > 0) {
+    return '$daysUntilSaturday dagar kvar till veckopeng!';
+  }
+  //borde bara hända på söndagar!
+  if (daysUntilSaturday < 0) {
+    return '6 dagar kvar till veckopeng!';
+  }
+  //borde bara hända på lördagar!
+  return 'Idag har du fått x veckopeng!';
 }
